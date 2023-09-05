cf3de0344c8b1d60b4ec619a80f1de830256c185
Курылёв Алексей <kurylev@effective-group.ru>
2023-09-05 17:50:37 +0300

    fix: import global style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
+import "./styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
-import "./styles/index.scss";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
