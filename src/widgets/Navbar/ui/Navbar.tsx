a99c5ab2bffd977fa3d2774d589534b151fa0371
Курылёв Алексей <kurylev@effective-group.ru>
2023-09-05 17:44:17 +0300

    feat: Navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,22 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import styles from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(styles.navbar, {}, [className])}>
+      <nav className={classNames(styles.nav)}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/">
+          Main
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+          About
+        </AppLink>
+      </nav>
+    </div>
+  );
+};
