c61debe0d8fd51aeb622b0d6671b89b385d1056f
Курылёв Алексей <kurylev@effective-group.ru>
2023-09-12 19:46:57 +0300

    add MemoryRouter

--- src/shared/lib/testUtils/testUtils.tsx
+++ src/shared/lib/testUtils/testUtils.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { render, RenderOptions } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import { i18nForTests } from "shared/config/i18n";
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => (
-  <I18nextProvider i18n={i18nForTests}>{children}</I18nextProvider>
+  <MemoryRouter>
+    <I18nextProvider i18n={i18nForTests}>{children}</I18nextProvider>
+  </MemoryRouter>
 );
 
 const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
