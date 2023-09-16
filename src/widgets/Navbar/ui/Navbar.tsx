4eeb9cca6080ce95cd4ae8c1d7b6339a12cb95eb
Курылёв Алексей <kurylev@effective-group.ru>
2023-09-16 22:06:40 +0300

    feat: Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,9 @@
+import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink";
+import { Button } from "shared/ui/Button";
+import { Modal } from "shared/ui/Modal/Modal";
 import styles from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -9,6 +12,11 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
+  const [isAuthModal, setIisAuthModal] = React.useState(false);
+
+  const onToggleModal = React.useCallback(() => {
+    setIisAuthModal((prev) => !prev);
+  }, []);
 
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
@@ -21,6 +29,12 @@ export const Navbar = ({ className }: NavbarProps) => {
           {t("О сайте")}
         </AppLink>
       </nav>
+      <Button variant='filled' size='s' onClick={onToggleModal}>
+        {t("Войти")}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        123
+      </Modal>
     </div>
   );
 };
