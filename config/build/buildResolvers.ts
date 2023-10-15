302fd998ef03cc45ab4db8a1980d1f6df240ba63
Курылёв Алексей <kurylev@effective-group.ru>
2023-10-15 19:34:46 +0300

    husky

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,10 +1,10 @@
-import { type ResolveOptions } from 'webpack';
-import { type BuildOptions } from './types/config';
+import { type ResolveOptions } from "webpack";
+import { type BuildOptions } from "./types/config";
 
 export const buildResolvers = (options: BuildOptions): ResolveOptions => ({
-    extensions: ['.tsx', '.ts', '.js'],
-    preferAbsolute: true,
-    modules: [options.paths.src, 'node_modules'],
-    mainFiles: ['index'],
-    alias: {},
+  extensions: [".tsx", ".ts", ".js"],
+  preferAbsolute: true,
+  modules: [options.paths.src, "node_modules"],
+  mainFiles: ["index"],
+  alias: {},
 });
